09c8632326c797505416cedb332ce39424012307
NicolasEngels <nicolasengels17@gmail.com>
2022-09-27 15:55:19 +0200

    plein de cool features

--- v-card/javascript/script.js
+++ v-card/javascript/script.js
@@ -3,3 +3,67 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+window.addEventListener('click', function(e){
+    if (!document.getElementById('nav').contains(e.target) && (!document.getElementById('menu-bar').contains(e.target)) && (!document.getElementById('menu-bg').contains(e.target))){
+        document.getElementById("menu-bar").classList.remove("change");
+        document.getElementById("nav").classList.remove("change");
+        document.getElementById("menu-bg").classList.remove("change-bg");
+  } 
+})
+
+function contact(){
+    document.getElementsByClassName("aboutme")[0].style.display = "none";
+    document.getElementById("baboutme").classList.remove("active");
+
+    document.getElementsByClassName("portfolio")[0].style.display = "none";
+    document.getElementById("bportfolio").classList.remove("active");
+
+    document.getElementsByClassName("resume")[0].style.display = "none";
+    document.getElementById("bresume").classList.remove("active");
+
+    document.getElementsByClassName("contact")[0].style.display = "block";
+    document.getElementById("bcontact").classList.add("active");
+}
+
+function aboutMe(){
+    document.getElementsByClassName("aboutme")[0].style.display = "block";
+    document.getElementById("baboutme").classList.add("active");
+
+    document.getElementsByClassName("portfolio")[0].style.display = "none";
+    document.getElementById("bportfolio").classList.remove("active");
+
+    document.getElementsByClassName("resume")[0].style.display = "none";
+    document.getElementById("bresume").classList.remove("active");
+
+    document.getElementsByClassName("contact")[0].style.display = "none";
+    document.getElementById("bcontact").classList.remove("active");
+}
+
+function portfolio(){
+    document.getElementsByClassName("aboutme")[0].style.display = "none";
+    document.getElementById("baboutme").classList.remove("active");
+
+    document.getElementsByClassName("portfolio")[0].style.display = "block";
+    document.getElementById("bportfolio").classList.add("active");
+
+    document.getElementsByClassName("resume")[0].style.display = "none";
+    document.getElementById("bresume").classList.remove("active");
+
+    document.getElementsByClassName("contact")[0].style.display = "none";
+    document.getElementById("bcontact").classList.remove("active");
+}
+
+function resume(){
+    document.getElementsByClassName("aboutme")[0].style.display = "none";
+    document.getElementById("baboutme").classList.remove("active");
+
+    document.getElementsByClassName("portfolio")[0].style.display = "none";
+    document.getElementById("bportfolio").classList.remove("active");
+
+    document.getElementsByClassName("resume")[0].style.display = "block";
+    document.getElementById("bresume").classList.add("active");
+
+    document.getElementsByClassName("contact")[0].style.display = "none";
+    document.getElementById("bcontact").classList.remove("active");
+}
