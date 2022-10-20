cf7a35fa805c85576ac21c634e3d15b8d1b29762
NicolasEngels <nicolasengels17@gmail.com>
2022-10-20 16:26:14 +0200

    move script

--- js-dom/7.Drill/drill.js
+++ js-dom/7.Drill/drill.js
@@ -1,3 +1,12 @@
 let list = document.querySelector("ul").childNodes;
 
-console.log(list);
+
+if (list[1].textContent != "Fast and Furious"){
+    for (elements in list){
+        if(list[elements].textContent == "Fast and Furious"){
+            const first = list[1];
+            list[elements].textContent = first.textContent;
+            list[1].textContent = "Fast and Furious";  
+        }
+    }
+}
