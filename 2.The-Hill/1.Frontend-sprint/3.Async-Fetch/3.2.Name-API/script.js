285bdc0a92c93eafb32f44287cb85e3093427a41
NicolasEngels <nicolasengels17@gmail.com>
2022-10-28 15:54:56 +0200

    some css

--- 2.The-Hill/1.Frontend-sprint/3.Async-Fetch/3.2.Name-API/script.js
+++ 2.The-Hill/1.Frontend-sprint/3.Async-Fetch/3.2.Name-API/script.js
@@ -20,26 +20,28 @@ fetch('./countries.json')
 button.addEventListener('click', () => {
     const name = document.getElementById('name');
     const country = document.getElementById('country').value;
+    const countryName = document.getElementById('country').value.textContent;
     const fetchName = name => fetch('https://api.agify.io/?name='+name+'&country_id='+country);
 
     fetchName(name.value)
         .then(response => response.json())
         .then(json => {
             const newName = document.createElement('div');
+            newName.classList.add("newName");
 
-            const nameValue = document.createElement('h4');
-            nameValue.textContent = name.value;
+            const nameValue = document.createElement('h3');
+            nameValue.textContent = "How many " + name.value.toUpperCase() + ' in ' + country;
             newName.appendChild(nameValue);
-
-            const nameAge = document.createElement('p');
-            nameAge.textContent = `median age : ${json.age}`;
-            newName.appendChild(nameAge);
 
             const nameCount = document.createElement('p');
             nameCount.textContent = `number of person with that name : ${json.count}`;
             newName.appendChild(nameCount);
 
-            document.body.appendChild(newName);
+            const nameAge = document.createElement('p');
+            nameAge.textContent = `median age : ${json.age}`;
+            newName.appendChild(nameAge);
+
+            document.querySelector("main").appendChild(newName);
         })
         .catch(error => {
             console.log('There was an error!', error)
